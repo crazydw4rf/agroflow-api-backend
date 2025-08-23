d3f20a17d4f2fcd8f0b678885141f7248e982510
Binar Nugroho <binarnugroho775@gmail.com>
2025-08-23 15:15:00 +0700

    feat: enhance ESLint configuration and add Prisma configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,32 @@
 import tseslint from "typescript-eslint";
+import unusedImportsPlugin from "eslint-plugin-unused-imports"
 import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config"
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
+  globalIgnores(["./experiments", "./src/generated", "./prisma", "*.*js"]),
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  {
+    plugins: {
+      "unused-imports": unusedImportsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // "no-unused-vars": "off"
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_",
+        },
+      ]
+    }
+  },
   {
     languageOptions: {
       parserOptions: {
@@ -15,7 +35,6 @@ export default tseslint.config(
       },
     },
   },
-  globalIgnores(["./experiments", "./src/generated", "*.*js"]),
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
@@ -34,6 +53,10 @@ export default tseslint.config(
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/require-await": "warn",
+      "@typescript-eslint/eol-last": "off",
+      "@typescript-eslint/space-before-function-paren": "off",
+      "space-before-function-paren": "off",
       "no-console": "warn",
     },
   }
